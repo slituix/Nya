e035547d8d0a52e282e0a1b10c1322f5122d2914
Saksham <saksham.mac@icloud.com>
2025-10-23 21:33:40 +0530

    Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,22 +10,39 @@ import {
   SlideToUnlockTrack,
 } from "@/components/slide-to-unlock";
 
-export default function SlideToUnlockDemo1() {
-
+export default function SlideToUnlockDemo2() {
   return (
-    <SlideToUnlock
-      onUnlock={() => {
-        toast.success("You are so cute nya");
-      }}
-    >
-      <SlideToUnlockTrack>
-        <SlideToUnlockText>
-          {({ isDragging }) => (
-            <ShimmeringText text="slide to unlock" isStopped={isDragging} />
-          )}
-        </SlideToUnlockText>
-        <SlideToUnlockHandle />
-      </SlideToUnlockTrack>
-    </SlideToUnlock>
+    <>
+      <SlideToUnlock
+        className="bg-gradient-to-b from-zinc-800 to-zinc-900"
+        onUnlock={() => {
+          const myPromise = new Promise((resolve) => {
+            setTimeout(() => {
+              resolve(true);
+            }, 1000);
+          });
+
+          toast.promise(myPromise, {
+            loading: "Connecting...",
+            success: () => `Connected`,
+            error: ({ message }) => `Error: ${message}`,
+          });
+        }}
+      >
+        <SlideToUnlockTrack>
+          <SlideToUnlockText>
+            {({ isDragging }) => (
+              <ShimmeringText
+                className="[--color:var(--color-zinc-600)] [--shimmering-color:var(--color-zinc-50)]"
+                text="slide to answer"
+                isStopped={isDragging}
+              />
+            )}
+          </SlideToUnlockText>
+
+          <SlideToUnlockHandle className="bg-gradient-to-b from-emerald-500 to-emerald-700 text-white" />
+        </SlideToUnlockTrack>
+      </SlideToUnlock>
+    </>
   );
 }
